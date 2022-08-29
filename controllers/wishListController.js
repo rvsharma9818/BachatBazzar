0559a70c01fed09f205f3dfc1b3b314e7684f04a
ravi kumar sharma <rvsharma2652@gmail.com>
2022-08-29 21:23:03 +0530

    Update wishListController.js

--- controllers/wishListController.js
+++ controllers/wishListController.js
@@ -92,7 +92,7 @@ const wishlistCreation = async (req, res) => {
             return res.status(200).send({ status: true, message: `Product added successfully`, data: responseData });
         }
     } catch (error) {
-        console.log(err)
+        console.log(error)
         res.status(500).send({ status: false, message: error});
     }
 };
