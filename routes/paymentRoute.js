97fed6d277154c9ccc64b3b7a8392d6956defba9
Ravi kumar sharma <rvsharma2652@gmail.com>
2022-09-05 19:46:20 +0530

    bugs fuixing

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -6,6 +6,7 @@ const { webhook, generatepayement } = require("../controllers/PaymentController"
 
 const { verifyTokenAndAuthorization } =require("../middleware/middleware")
 
+const create = require("./orderController")
 
 router.route("/create-checkout-session/:userId").post(verifyTokenAndAuthorization,generatepayement);
 
@@ -13,6 +14,55 @@ router.route("/create-checkout-session/:userId").post(verifyTokenAndAuthorizatio
 
 // Stripe webhoook
 
-router.post("/webhook", express.json({ type: "application/json" }), webhook);
+router.post("/webhook", express.json({ type: "application/json" }), (async (req, res) => {
+    async (req, res) => {
+        let data;
+        let eventType;
+
+        // Check if webhook signing is configured.
+        let webhookSecret;
+        //webhookSecret = process.env.STRIPE_WEB_HOOK;
+
+        if (webhookSecret) {
+            // Retrieve the event by verifying the signature using the raw body and secret.
+            let event;
+            let signature = req.headers["stripe-signature"];
+
+            try {
+                event = Stripe.webhooks.constructEvent(
+                    req.body,
+                    signature,
+                    webhookSecret
+                );
+
+            } catch (err) {
+                console.log(`⚠️  Webhook signature verification failed:  ${err}`);
+                return res.sendStatus(400);
+            }
+            data = event.data.object;
+            eventType = event.type;
+        } else {
+            data = req.body.data.object;
+            eventType = req.body.type;
+        }
+
+        if (eventType === "checkout.session.completed") {
+            Stripe.customers
+                .retrieve(data.customer)
+                .then(async (customer) => {
+                    try {
+                        // CREATE ORDER
+                        create.createOrder(customer, data);
+                    } catch (err) {
+                        console.log(typeof createOrder);
+                        console.log(err);
+                    }
+                })
+                .catch((err) => console.log(err.message));
+        }
+
+        res.status(200).end();
+    }
+}));
 
 module.exports = router;
